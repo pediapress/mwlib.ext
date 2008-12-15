88591d0ad82e0d28e520b8f18ea9c509223a94fa
johannes.beigel@brainbot.com <johannes.beigel@brainbot.com>
2008-12-15 16:51:34 +0100

    adjusted upate.sh for new reportlab dir layout. made manifest.

--- update.sh
+++ update.sh
@@ -4,8 +4,8 @@ rm -rf reportlab
 R=../reportlab
 hg clone $R reportlab
 id=$(hg ident -i reportlab/)
-rsync -aH --delete reportlab/reportlab mwlib/
-rsync -aH --delete reportlab/rl_addons mwlib/
+rsync -aH --delete reportlab/src/reportlab mwlib/
+rsync -aH --delete reportlab/src/rl_addons mwlib/
 hg addremove mwlib/reportlab
 hg addremove mwlib/rl_addons
 
