ae3f2f00edc05b4500b6a8e968e146e744c711a1
Ralf Schmitt <ralf@systemexit.de>
2011-10-12 11:40:36 +0200

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 #! /usr/bin/env python
 
-import sys
-import os
-
-import ez_setup
-ez_setup.use_setuptools()
-
-from setuptools import setup, Extension, find_packages
-import distutils.util
+try:
+    from setuptools import setup, Extension, find_packages
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, Extension, find_packages
 
 from distutils import sysconfig
 if sysconfig.get_config_var("LIBM") == "-lm":
@@ -15,37 +13,30 @@ if sysconfig.get_config_var("LIBM") == "-lm":
 else:
     libraries = []
 
-
-install_requires=[]
-execfile(distutils.util.convert_path('mwlib/_extversion.py')) 
-# adds 'version' to local namespace
-
-def read_long_description():
-    fn = os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.txt")
-    return open(fn).read()
+install_requires = []
+execfile('mwlib/_extversion.py')  # adds 'version' to local namespace
 
 ext_modules = []
 ext_modules.append(Extension("mwlib.ext._rl_accel",
                              ['upstream-src/src/rl_addons/rl_accel/_rl_accel.c'],
                              libraries=libraries))
 
-packages = ["mwlib.ext."+x for x in find_packages("upstream-src/src")]+find_packages(".")
+packages = ["mwlib.ext." + x for x in find_packages("upstream-src/src")] + find_packages(".")
 
 setup(
     name="mwlib.ext",
     version=str(version),
-    install_requires=install_requires,    
-    packages=packages, 
+    install_requires=install_requires,
+    packages=packages,
     package_dir={"mwlib.ext.reportlab": "upstream-src/src/reportlab",
                  "mwlib.ext.rl_addons": "upstream-src/src/rl_addons"},
     ext_modules=ext_modules,
     namespace_packages=['mwlib'],
-    include_package_data = True,
-    zip_safe = False,
-    url = "http://code.pediapress.com/",
+    include_package_data=True,
+    zip_safe=False,
+    url="http://code.pediapress.com/",
     description="provide dependencies for mwlib",
     license="BSD License",
     maintainer="pediapress.com",
     maintainer_email="info@pediapress.com",
-    long_description = read_long_description()
-)
+    long_description=open("README.txt").read())
